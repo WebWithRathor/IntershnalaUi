e126fd54fca4dc45d5c626b694ffbb007bedcd90
WebWithRathor <devrajrathor143@gmail.com>
2024-08-06 22:32:06 +0530

    .

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -2,13 +2,16 @@ import React, { useEffect } from 'react'
 import Nav from '../Partials/Nav'
 import { Link, Outlet, useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux';
+import { Instance } from '../../utils/Axios';
 
 const Resume = () => {
     const navigate = useNavigate();
     const student = useSelector(store => store.studentSlice.student)
-    useEffect(() => {
-        console.log(student && student.resume.experience);
-    }, [student])
+
+    const DeleteHandler = (e,{target},type)=>{
+        target.parentNode.parentNode.parentNode.removeChild(target.parentNode.parentNode);
+         Instance.post(`/resume/delete-${type}/${e}`);
+    }
 
     return (
         <>
@@ -38,8 +41,8 @@ const Resume = () => {
                                             <h2 className='text-gray-600'>{e.degree} {e.stream} <span className='text-sm'>{e.result != '' ? ("( " + e.result + " " + e.resultType + " )") : ''}</span></h2>
                                             <h2 className='text-gray-600'>{e.startYear} - {e.endYear}</h2>
                                             <div className="icons flex gap-3 absolute right-5 top-2">
-                                                <i className="fa-solid fa-pen-to-square text-gray-600"></i>
-                                                <i className="fa-solid fa-trash text-gray-600"></i>
+                                                <i className="fa-solid fa-pen-to-square hover:text-gray-900 text-gray-600 cursor-pointer"></i>
+                                                <i onClick={(elem)=>DeleteHandler(e.id,elem,'edu')} className="fa-solid fa-trash hover:text-gray-900 text-gray-600 cursor-pointer"></i>
                                             </div>
                                         </div>
                                     })}
@@ -51,13 +54,13 @@ const Resume = () => {
                                 <h1 className='w-1/3 text-xs text-gray-600 font-medium uppercase' >work experience</h1>
                                 <div className="details flex-1 space-y-4">
                                     {student.resume.experience.map((e, i) => {
-                                        return <div className="dets relative">
+                                        return <div key={e.id} className="dets relative">
                                             <h1 className='font-semibold'>{e.profile} ({e.type})</h1>
                                             <h2 className='text-gray-600'>{e.organizationName} ( {e.location} ) </h2>
                                             <h2 className='text-gray-600'>{e.startYear} - {e.endYear}</h2>
                                             <div className="icons flex gap-3 absolute right-5 top-2">
-                                                <i className="fa-solid fa-pen-to-square text-gray-600"></i>
-                                                <i className="fa-solid fa-trash text-gray-600"></i>
+                                                <i className="fa-solid fa-pen-to-square hover:text-gray-900 text-gray-600 cursor-pointer"></i>
+                                                <i onClick={(elem)=>DeleteHandler(e.id,elem,'exp')} className="fa-solid fa-trash hover:text-gray-900 text-gray-600 cursor-pointer"></i>
                                             </div>
                                         </div>
                                     })}
@@ -71,37 +74,104 @@ const Resume = () => {
                             <div className="position border-b flex py-7">
                                 <h1 className='w-1/3 text-xs text-gray-600 font-medium uppercase' >position of responsibility</h1>
                                 <div className="details flex-1 space-y-2">
-                                    <Link className='flex items-center gap-1 text-sm font-medium text-blue-600 w-max'><i className="fa-solid fa-plus -mb-0.5"></i>Add position of responsibility</Link>
+                                    <ul className='list-disc ml-5 space-y-4'>
+                                        {student.resume.responsibility.map((e, i) => {
+                                            return <div key={i} className='relative'>
+                                                <li  className='-mt-1.5 font-medium text-gray-700' >{e.description}</li>
+                                                <div className="icons flex gap-3 absolute right-5 top-2">
+                                                    <i className="fa-solid fa-pen-to-square hover:text-gray-900 text-gray-600 cursor-pointer"></i>
+                                                    <i onClick={(elem)=>DeleteHandler(e.id,elem,'resp')} className="fa-solid fa-trash hover:text-gray-900 text-gray-600 cursor-pointer"></i>
+                                                </div>
+                                            </div>
+                                        })}
+                                    </ul>
+                                    <Link to='responsibility' className='flex items-center gap-1 text-sm font-medium text-blue-600 w-max'><i className="fa-solid fa-plus -mb-0.5"></i>Add position of responsibility</Link>
                                 </div>
                             </div>
                             <div className="training border-b flex py-7">
                                 <h1 className='w-1/3 text-xs text-gray-600 font-medium uppercase' >training / courses</h1>
                                 <div className="details flex-1 space-y-2">
-                                    <Link className='flex items-center gap-1 text-sm font-medium text-blue-600 w-max'><i className="fa-solid fa-plus -mb-0.5"></i>Add training / courses</Link>
+                                    {student.resume.courses.map((e, i) => {
+                                        return <div key={e.id} className="dets relative">
+                                            <h1 className='font-semibold'>{e.profile}</h1>
+                                            <h2 className='text-gray-600'>{e.organizationName} ( {e.location} ) </h2>
+                                            <h2 className='text-gray-600'>{e.startYear} - {e.endYear}</h2>
+                                            <div className="icons flex gap-3 absolute right-5 top-2">
+                                                <i className="fa-solid fa-pen-to-square hover:text-gray-900 text-gray-600 cursor-pointer"></i>
+                                                <i onClick={(elem)=>DeleteHandler(e.id,elem,'cour')} className="fa-solid fa-trash hover:text-gray-900 text-gray-600 cursor-pointer"></i>
+                                            </div>
+                                        </div>
+                                    })}
+                                    <Link to='training' className='flex items-center gap-1 text-sm font-medium text-blue-600 w-max'><i className="fa-solid fa-plus -mb-0.5"></i>Add training / courses</Link>
                                 </div>
                             </div>
                             <div className="academics border-b flex py-7">
                                 <h1 className='w-1/3 text-xs text-gray-600 font-medium uppercase' >ACADEMICS/ <br /> PERSONAL PROJECTS</h1>
                                 <div className="details flex-1 space-y-2">
-                                    <Link className='flex items-center gap-1 text-sm font-medium  text-blue-600 w-max '><i className="fa-solid fa-plus -mb-0.5"></i>Add <span className='lowercase'>ACADEMICS/ PERSONAL PROJECTS</span></Link>
+                                    {student.resume.projects.map((e, i) => {
+                                        return <div key={e.id} className="dets relative">
+                                            <a href={e.URL} target='_blank' className='font-semibold'>{e.Title}</a>
+                                            <p className='text-gray-600 w-3/4 text-sm mb-1'>{e.description}</p>
+                                            <h2 className='text-gray-600'>{e.startMonth} - {e.endMonth}</h2>
+                                            <div className="icons flex gap-3 absolute right-5 top-2">
+                                                <i className="fa-solid fa-pen-to-square hover:text-gray-900 text-gray-600 cursor-pointer"></i>
+                                                <i onClick={(elem)=>DeleteHandler(e.id,elem,'pro')} className="fa-solid fa-trash hover:text-gray-900 text-gray-600 cursor-pointer"></i>
+                                            </div>
+                                        </div>
+                                    })}
+                                    <Link to='projects' className='flex items-center gap-1 text-sm font-medium  text-blue-600 w-max '><i className="fa-solid fa-plus -mb-0.5"></i>Add <span className='lowercase'>ACADEMICS/ PERSONAL PROJECTS</span></Link>
                                 </div>
                             </div>
                             <div className="skill border-b flex py-7">
                                 <h1 className='w-1/3 text-xs text-gray-600 font-medium uppercase' >SKILLS</h1>
-                                <div className="details flex-1 space-y-2">
-                                    <Link className='flex items-center gap-1 text-sm font-medium  text-blue-600 w-max '><i className="fa-solid fa-plus -mb-0.5"></i>Add <span className='lowercase'>SKILLS</span></Link>
+                                <div className="details flex-1 ">
+                                    <div className="flex gap-2 -mt-2 mb-3 items-center">
+
+                                        {student.resume.skills.map((e, i) => {
+                                            return <button key={i}
+                                                className="flex gap-3 items-center justify-center text-gray-700 w-max py-1.5 px-3 bg-gray-200 rounded-md hover:bg-gray-300 focus:outline-none"
+                                            >
+                                                {e.Skill}<div className="icons flex gap-2 ">
+                                                    <i className="fa-solid text-sm fa-pen-to-square text-gray-500 hover:text-gray-800"></i>
+                                                    <i onClick={(elem)=>DeleteHandler(e.id,elem,'skill')}  className="fa-solid text-sm fa-trash text-gray-500 hover:text-gray-800"></i>
+                                                </div>
+                                            </button>
+                                        })}
+                                    </div>
+                                    <Link to='skill' className='flex items-center gap-1 text-sm font-medium  text-blue-600 w-max '><i className="fa-solid fa-plus -mb-0.5"></i>Add <span className='lowercase'>SKILLS</span></Link>
                                 </div>
                             </div>
                             <div className="portfolio border-b flex py-7">
                                 <h1 className='w-1/3 text-xs text-gray-600 font-medium uppercase' >PORTFOLIO / <br /> WORK SAMPLES</h1>
-                                <div className="details flex-1 space-y-2">
-                                    <Link className='flex items-center gap-1 text-sm font-medium  text-blue-600 w-max '><i className="fa-solid fa-plus -mb-0.5"></i>Add <span className='lowercase'>PORTFOLIO/ WORK SAMPLES</span></Link>
+                                <div className="details flex-1 space-y-3">
+                                    {student.resume.workSamples.map((e, i) => {
+                                        return <div key={e.id} className="dets relative">
+                                            <h1 className='font-semibold'> <i className={`${e.WorkIcon} mr-1`} ></i> {e.WorkName}</h1>
+                                            <a href={e.url} className='text-sm text-blue-600'>{e.url}</a>
+                                            <div className="icons flex gap-3 absolute right-5 top-2">
+                                                <i className="fa-solid fa-pen-to-square hover:text-gray-900 text-gray-600 cursor-pointer"></i>
+                                                <i onClick={(elem)=>DeleteHandler(e.id,elem,'work')} className="fa-solid fa-trash hover:text-gray-900 text-gray-600 cursor-pointer"></i>
+                                            </div>
+                                        </div>
+                                    })}
+                                    <Link to='worksample' className='flex items-center gap-1 text-sm font-medium  text-blue-600 w-max '><i className="fa-solid fa-plus -mb-0.5"></i>Add <span className='lowercase'>PORTFOLIO/ WORK SAMPLES</span></Link>
                                 </div>
                             </div>
                             <div className="accomplishment border-b flex py-7">
                                 <h1 className='w-1/3 text-xs text-gray-600 font-medium uppercase' >ACCOMPLISHMENTS/ <br /> ADDITIONAL DETAILS</h1>
                                 <div className="details flex-1 space-y-2">
-                                    <Link className='flex items-center gap-1 text-sm font-medium  text-blue-600 w-max '><i className="fa-solid fa-plus -mb-0.5"></i>Add <span className='lowercase'>ACCOMPLISHMENTS/ ADDITIONAL DETAILS</span></Link>
+                                    <ul className='list-disc ml-5 space-y-4'>
+                                        {student.resume.accomplishment.map((e, i) => {
+                                            return <div key={e.id} className='relative'>
+                                                <li  className='-mt-1.5 font-medium text-gray-700' >{e.description}</li>
+                                                <div className="icons flex gap-3 absolute right-5 top-2">
+                                                    <i className="fa-solid fa-pen-to-square hover:text-gray-900 text-gray-600 cursor-pointer"></i>
+                                                    <i onClick={(elem)=>DeleteHandler(e.id,elem,'accomp')} className="fa-solid fa-trash hover:text-gray-900 text-gray-600 cursor-pointer"></i>
+                                                </div>
+                                            </div>
+                                        })}
+                                    </ul>
+                                    <Link to='accomplishment' className='flex items-center gap-1 text-sm font-medium  text-blue-600 w-max '><i className="fa-solid fa-plus -mb-0.5"></i>Add <span className='lowercase'>ACCOMPLISHMENTS/ ADDITIONAL DETAILS</span></Link>
                                 </div>
                             </div>
                         </div>
