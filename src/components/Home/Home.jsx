05b2384a63319f066237d9f24b800b456467ba85
WebWithRathor <devrajrathor143@gmail.com>
2024-08-10 21:42:52 +0530

    .

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -16,33 +16,29 @@ const HomeLayout = () => {
   const student = useSelector(store => store.studentSlice.student);
   const employe = useSelector(store => store.employeSlice.employe);
 
-  
+
   useEffect(() => {
     if (type === 'student') {
-
       if (student === null) dispatch(loadStudent(navigate));
-
-    } else {
+    } else if (type === 'employe') {
 
       if (employe === null) dispatch(loadEmploye(navigate));
-      console.log(employe);
-      
-      if(employe && !employe.completeDetails) navigate('/employe/completeDetails')
+      if (employe && !employe.completeDetails) navigate('/employe/completeDetails')
 
     }
-  }, [student]);
+  }, [student, employe]);
 
 
 
   return (
     <div className='h-full pt-32 w-full'>
-      <Nav isLoggedIn={isLoggedIn} student={student} />
+      <Nav isLoggedIn={isLoggedIn} student={student} employe={employe} />
 
       <div className="">
         <h1 className='text-[3vw] text-zinc-700 font-bold text-center mt-10'>{student ? 'Make your dream career a reality' : 'Make your dream a reality'}</h1>
         <img className='h-14 mx-auto w-[35vw] pl-40 object-[-27px_1px]' src="https://internshala.com/static/images/home/sprites/underline_fire.png" alt="" />
       </div>
-      {isLoggedIn && student != null  || employe != null ?
+      {isLoggedIn && student != null || employe != null ?
         <h1 className='text-center text-zinc-700 font-bold text-3xl'>Trending for <span className='capitalize'>{student && student.firstname || employe.firstname} {student && student.lastname || employe.lastname}</span>🔥</h1> :
         <h1 className='text-center text-zinc-700 font-bold text-3xl'>Trending on Intershala 🔥</h1>
       }
--- src/components/Partials/Nav.jsx
+++ src/components/Partials/Nav.jsx
@@ -4,8 +4,9 @@ import NavLoginRight from './NavLoginRight'
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { loadStudent } from '../../store/actions/studentAction';
+import EmployeNavLogin from './EmployeNavLogin.jsx';
 
-const Nav = ({isLoggedIn,student}) => {
+const Nav = ({isLoggedIn,student ,employe}) => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { pathname } = useLocation()
@@ -20,7 +21,7 @@ const Nav = ({isLoggedIn,student}) => {
   return (
     <div className='w-full fixed top-0 left-0 px-44 flex items-center justify-between bg-white z-[9] shadow border-b'>
       <div className="left flex items-center">
-        <Link to='/'><img className='h-10 -mt-2 mr-10' src={`https://upload.wikimedia.org/wikipedia/en/8/8b/Internshala_company_logo.png`} alt="" /></Link>
+        <Link to='/'><img className='h-10  -mt-2 mr-10' src={`https://upload.wikimedia.org/wikipedia/en/8/8b/Internshala_company_logo.png`} alt="" /></Link>
         {!isLoggedIn &&
           <>
             <h1 className=" hover:bg-blue-50 h-full py-5 px-4 cursor-pointer font-medium group">Internships <i className="fa-solid fa-caret-down  group-hover:animate-[bounce_1s_linear_1.5] ml-1 fa-md"></i></h1>
@@ -28,8 +29,8 @@ const Nav = ({isLoggedIn,student}) => {
           </>
         }
       </div>
-      {isLoggedIn ?
-        <NavLoginRight student={student} /> :
+      {isLoggedIn && student ?
+        <NavLoginRight student={student} /> : employe ? <EmployeNavLogin employe={employe} />:
         <NavRight />
       }
     </div>
